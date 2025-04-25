fa2bbb453316000157908de4f7c0e9254252137e
Charly <magicstyl3@gmail.com>
2025-04-24 23:41:04 -0300

    feat: swagger integration + get endpoint products

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  // — Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Kansaco 2025 API')
+    .setDescription('Endpoints to manage Productos, Pedidos, etc.')
+    .setVersion('1.0')
+    .addBearerAuth({ type: 'http', scheme: 'bearer' }, 'access-token')
+    .build();
+
+  const document = SwaggerModule.createDocument(app as any, config);
+  // serve at /docs by default (or /api/docs if you have a prefix)
+  SwaggerModule.setup('docs', app as any, document, {
+    swaggerOptions: { persistAuthorization: true },
   });
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,66 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Logger, Query, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ProductoService } from './product.service';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { ProductGet } from './dto/productGet.dto';
+import { ProductResponse } from './dto/productResponse.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('producto')
-export class ProductoController {}
+@ApiTags('Kansaco - Productos')
+export class ProductoController {
+  protected logger = new Logger('ProductoController');
+  constructor(private readonly productoService: ProductoService) {}
+
+  @Get()
+  // @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  async getAllProducts() {
+    return this.productoService.getAllProducts();
+  }
+
+  @Get('/filter')
+  // @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiQuery({ name: 'category', required: false })
+  @ApiQuery({ name: 'name', required: false })
+  @ApiQuery({ name: 'sku', required: false })
+  @ApiQuery({ name: 'stock', required: false })
+  @ApiQuery({ name: 'wholeSaler', required: false })
+  @ApiQuery({ name: 'isVisible', required: false })
+  async getFilteredProducts(
+    @Query('category') category?: string,
+    @Query('name') name?: string,
+    @Query('sku') sku?: string,
+    @Query('stock') stock?: string,
+    @Query('wholeSaler') wholeSaler?: string,
+    @Query('isVisible') isVisible?: string,
+  ): Promise<ProductResponse[]> {
+    try {
+      const filters: Partial<Record<keyof ProductGet, string>> = {
+        category,
+        name,
+        sku,
+        stock,
+        wholeSaler,
+        isVisible,
+      };
+      const filteredProducts =
+        await this.productoService.getFilteredProducts(filters);
+      return filteredProducts.length > 0
+        ? filteredProducts.map((product) =>
+            plainToInstance(ProductResponse, product),
+          )
+        : [];
+    } catch (error) {
+      this.logger.error(error);
+    }
+  }
+}
